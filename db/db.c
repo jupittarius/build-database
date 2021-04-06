a101bec93f840089b00d9efb42b4a24c74b2a819
jupittarius <presdell@outlook.com>
2021-04-06 21:21:28 +0800

    B-Tree Leaf Node Format

--- db/db.c
+++ db/db.c
@@ -37,18 +37,41 @@ const uint32_t ROW_SIZE = ID_SIZE + USERNAME_SIZE + EMAIL_SIZE;
 const uint32_t PAGE_SIZE = 4096;
 #define TABLE_MAX_PAGES 100
 
-const uint32_t ROWS_PER_PAGE = PAGE_SIZE / ROW_SIZE;
-const uint32_t TABLE_MAX_ROWS = ROWS_PER_PAGE * TABLE_MAX_PAGES;
+
+
+// Common Node Header Layout
+const uint32_t NODE_TYPE_SIZE = sizeof(uint8_t);
+const uint32_t NODE_TYPE_OFFSET = 0;
+const uint32_t IS_ROOT_SIZE = sizeof(uint8_t);
+const uint32_t IS_ROOT_OFFSET = NODE_TYPE_SIZE;
+const uint32_t PARENT_POINTER_SIZE = sizeof(uint32_t);
+const uint32_t PARENT_POINTER_OFFSET = IS_ROOT_OFFSET + IS_ROOT_SIZE;
+const uint8_t COMMON_NODE_HEADER_SIZE = NODE_TYPE_SIZE + IS_ROOT_SIZE + PARENT_POINTER_SIZE;
+
+// Leaf Node Header Layout
+const uint32_t LEAF_NODE_NUM_CELLS_SIZE = sizeof(uint32_t);
+const uint32_t LEAF_NODE_NUM_CELLS_OFFSET = COMMON_NODE_HEADER_SIZE;
+const uint32_t LEAF_NODE_HEADER_SIZE = COMMON_NODE_HEADER_SIZE + LEAF_NODE_NUM_CELLS_SIZE;
+
+// Leaf Node Body Layout
+const uint32_t LEAF_NODE_KEY_SIZE = sizeof(uint32_t);
+const uint32_t LEAF_NODE_KEY_OFFSET = 0;
+const uint32_t LEAF_NODE_VALUE_SIZE = ROW_SIZE;
+const uint32_t LEAF_NODE_VALUE_OFFSET = LEAF_NODE_KEY_OFFSET + LEAF_NODE_KEY_SIZE;
+const uint32_t LEAF_NODE_CELL_SIZE = LEAF_NODE_KEY_SIZE + LEAF_NODE_VALUE_SIZE;
+const uint32_t LEAF_NODE_SPACE_FOR_CELLS = PAGE_SIZE - LEAF_NODE_HEADER_SIZE;
+const uint32_t LEAF_NODE_MAX_CELLS = LEAF_NODE_SPACE_FOR_CELLS / LEAF_NODE_CELL_SIZE;
 
 typedef struct {
     int file_descriptor;
     uint32_t file_length;
+    uint32_t num_pages;
     void *pages[TABLE_MAX_PAGES];
 } Pager;
 
 typedef struct {
     Pager *pager;
-    uint32_t num_rows;
+    uint32_t root_page_num;
 } Table;
 
 
@@ -70,6 +93,10 @@ typedef enum {
     EXECUTE_SUCCESS, EXECUTE_DUPLICATE_KEY, EXECUTE_TABLE_FULL
 } ExecuteResult;
 
+typedef enum {
+    NODE_INTERNAL,
+    NODE_LEAF
+} NodeType;
 
 typedef struct {
     StatementType type;
@@ -78,9 +105,30 @@ typedef struct {
 
 typedef struct {
     Table* table;
-    uint32_t row_num;
+    uint32_t page_num;
+    uint32_t cell_num;
     bool end_of_table;      //Indicates a position one past the last element
 }Cursor;
+
+uint32_t* leaf_node_num_cells(void* node){
+    return node + LEAF_NODE_NUM_CELLS_OFFSET;
+}
+
+void* leaf_node_cell(void* node, uint32_t cell_num) {
+    return node + LEAF_NODE_HEADER_SIZE + cell_num * LEAF_NODE_CELL_SIZE;
+}
+
+uint32_t* leaf_node_key(void* node, uint32_t cell_num) {
+    return leaf_node_cell(node, cell_num);
+}
+
+void* leaf_node_value(void* node, uint32_t cell_num) {
+    return leaf_node_cell(node, cell_num) + LEAF_NODE_KEY_SIZE;
+}
+
+void initialize_leaf_node(void* node) {
+    *leaf_node_num_cells(node) = 0;
+}
 
 Cursor* table_start(Table* table){
     Cursor* cursor = malloc(sizeof(Cursor));
@@ -143,6 +191,11 @@ void* get_page(Pager* pager, uint32_t page_num){
         }
 
         pager->pages[page_num] = page;
+
+        if (page_num >= pager->num_pages) {
+            pager->num_pages = page_num + 1;
+        }
+        
     }
 
     return pager->pages[page_num];
@@ -150,7 +203,7 @@ void* get_page(Pager* pager, uint32_t page_num){
 
 }
 
-void pager_flush(Pager* pager, uint32_t page_num, uint32_t size){
+void pager_flush(Pager* pager, uint32_t page_num){
     if (pager->pages[page_num] == NULL){
         printf("Tried to flush null page.\n");
         exit(EXIT_FAILURE);
@@ -164,7 +217,7 @@ void pager_flush(Pager* pager, uint32_t page_num, uint32_t size){
     }
 
     ssize_t bytes_written =
-            write(pager->file_descriptor, pager->pages[page_num], size);
+            write(pager->file_descriptor, pager->pages[page_num], PAGE_SIZE);
 
     if (bytes_written == -1){
         printf("Error writing.\n", errno);
@@ -174,26 +227,13 @@ void pager_flush(Pager* pager, uint32_t page_num, uint32_t size){
 
 void db_close(Table* table){
     Pager* pager = table->pager;
-    uint32_t num_full_pages = table->num_rows / ROWS_PER_PAGE;
 
-    for (uint32_t i = 0; i < num_full_pages; i++) {
+    for (uint32_t i = 0; i < pager->num_pages; i++) {
         if (pager->pages[i] == NULL){
             continue;
         }
 
-        pager_flush(pager, i, PAGE_SIZE);
-    }
-
-    uint32_t num_additional_rows = table->num_rows % ROWS_PER_PAGE;
-
-    if (num_additional_rows > 0){
-        uint32_t page_num = num_full_pages;
-        if (pager->pages[page_num] != NULL){
-            pager_flush(pager, page_num, num_additional_rows*ROW_SIZE);
-
-            free(pager->pages[page_num]);
-            pager->pages[page_num] = NULL;
-        }
+        pager_flush(pager, i);
     }
 
     int result = close(pager->file_descriptor);
@@ -259,6 +299,14 @@ Pager *pager_open(const char *filename) {
     Pager* pager = malloc(sizeof(Pager));
     pager->file_descriptor = fd;
     pager->file_length = file_length;
+    pager->num_pages = (file_length/PAGE_SIZE);
+
+    if (file_length % PAGE_SIZE != 0)
+    {
+        printf("Db file is not a whole number of pages. Corrupt file.\n");
+        exit(EXIT_FAILURE);
+    }
+    
 
     for (uint32_t i = 0; i < TABLE_MAX_PAGES; i++) {
         pager->pages[i] = NULL;
